4ae5d545425010986d7f646c2d75bf2ab07c1a7c
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-08-22 19:02:08 +0700

    add search products and fix some nav items

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import htmx from 'astro-htmx';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  integrations: [tailwind(),htmx()],
 });
