c51219c94521b23023b53f1237be2069513003a4
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-08-22 19:08:19 +0700

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import htmx from 'astro-htmx';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(),htmx()],
+  integrations: [tailwind(), htmx()],
+  adapter: netlify()
 });
